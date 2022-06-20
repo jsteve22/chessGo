7801dc164e14682d78c85d18fa4341a671ba225e
Jeremy Stevens <jsteve22@nd.edu>
2022-06-19 20:45:01 -0400

    Updated with Pseudo-Moves

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
-import "github.com/jsteve22/chessGo/board"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/jsteve22/chessGo/board"
+)
 
 func main() {
 	// this is a comment
 	var b = board.InitBoard()
 
-	b.FENSet("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R")
+	b.FENSet("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQK2R")
 
-	b.PrintBoard()
+
+	var nextMove string
+
+	for {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+
+		b.PrintBoard()
+
+		b.GenMoves()
+
+		fmt.Scan(&nextMove)
+
+		b.MakeMove(nextMove)
+	}
 }
