58f799187ec4647bcb55ca9cc8b185e3cd12063d
Jeremy Stevens <jsteve22@nd.edu>
2022-06-24 11:36:50 -0400

    Update playing interaction and undoing moves

--- board/board.go
+++ board/board.go
@@ -7,6 +7,11 @@ import (
 type Move struct {
 	start int8
 	end int8
+	pieceMoved *Piece			// pointer to piece that moved
+	pieceCaptured *Piece	// pointer if a piece was taken
+	capPos int8						// position where the piece was captured
+	castle uint8					// 1=K; 2=Q; 3=k; 4=q
+	color uint8 					// color of move
 }
 
 type Piece struct {
@@ -23,10 +28,13 @@ type ChessBoard struct {
 	black [16]Piece
 
 	moves []Move
+	prevMoves []Move
+	attackSquares []int8
 
-	nextmove int
+	nextmove uint8
 	turn uint
 	revcnt uint
+	check bool
 
 	castle [4]bool	// KQkq
 	enpas int8
@@ -36,31 +44,68 @@ func Aboard() {
 	fmt.Printf("board test")
 }
 
-func InitBoard() ChessBoard {
-	var newCB ChessBoard
+func (cb *ChessBoard) InitBoard() {
 	
-	newCB.white = initSide(0)
-	newCB.black = initSide(1)	
+	cb.white = initSide(0)
+	cb.black = initSide(1)	
 
-	newCB.FENSet("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+	cb.castle[0] = true
+	cb.castle[1] = true
+	cb.castle[2] = true
+	cb.castle[3] = true
+	cb.enpas = -1
 
-	newCB.castle[0] = true
-	newCB.castle[1] = true
-	newCB.castle[2] = true
-	newCB.castle[3] = true
+	cb.prevMoves = make([]Move, 0)
+	cb.nextmove = 0
+	cb.turn = 1
+	cb.revcnt = 0
 
-	newCB.nextmove = 0
-	newCB.turn = 1
-	newCB.revcnt = 0
+	cb.FENSet("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
 
-	return newCB
 }
 
 func (cb *ChessBoard) UpdateNextMove() {
 	cb.nextmove = cb.nextmove ^ 1
+	// cb.enpas = -1
+	// fmt.Printf("lol\n")
+
+	// go through each side and check if a pawn is on the back rank
+	// so that it can promote to a new piece
+	var rank int8
+	// go through the white pieces
+	for i,p := range cb.white {
+		rank = ( p.pos & 56 ) >> 3
+		if rank == 7 && p.piece == 1 && p.alive {
+			pawnPromote( &cb.white[i] )
+		}
+	}
+
+	// go through the black pieces
+	for i,p := range cb.black {
+		rank = ( p.pos & 56 ) >> 3
+		if rank == 0 && p.piece == 1 && p.alive {
+			pawnPromote( &cb.black[i] )
+		}
+	}
 }
 
-func (cb *ChessBoard) MakeMove(move string) {
+func pawnPromote(p *Piece) {
+	// this function will update a pawn to a different piece
+	// this will also update the rep of the piece
+
+	// for now this will only upgrade pawns to queens
+
+	// change piece
+	p.piece = 5
+
+	if p.color == 0 {
+		p.rep = 'Q'
+	} else if p.color == 1 {
+		p.rep = 'q'
+	}
+}
+
+func (cb *ChessBoard) UserMove(move string) bool {
 	// this function will make a move on the board 
 	sf := (int8)(move[0] - 'A')
 	sr := (int8)(move[1] - '1')
@@ -71,16 +116,317 @@ func (cb *ChessBoard) MakeMove(move string) {
 	st := sf + (sr << 3)
 	en := ef + (er << 3)
 
+	for _,m := range cb.moves {
+		var nMove Move
+		nMove.start = st
+		nMove.end = en
+		if nMove.start == m.start && nMove.end == m.end {
+			cb.permanentMove( nMove )
+			return true
+		}
+	}
+	return false
+	/*
+		// cb.makeMove( Move{st, en} )
+		cb.permanentMove( Move{st, en} )
+		return true
+	*/
+}
+
+func (cb *ChessBoard) permanentMove (move Move) {
+	// this function will make a move that won't be 
+	// undone. This move will permanently delete pieces
+	// if taken 
+
+	st := move.start
+	en := move.end 
+
 	if cb.board[en] != nil {
-		cb.board[en].alive = false
+		// this will move the piece completely off board
+		cb.board[en].pos = -1
+
 	}
 
+	// this will check if piece moved is pawn
+	if cb.board[st].piece == 1 {
+		// this will check if the move was an enpas take
+		if en == cb.enpas {
+			stRank := ( st & 56 ) >> 3
+			enFile := en & 7 
+			enpasPawnPos := (stRank << 3) + enFile
+			cb.board[ enpasPawnPos ].pos = -1;
+		}
+	}
+
+	// call make move to perform the move since the piece
+	// has already been removed from board
+	cb.makeMove( move )
+}
+
+func (cb *ChessBoard) makeMove(move Move) {
+	// this function will make a move and update prevMove
+	st := move.start
+	en := move.end
+
+	// create appendMove which will be added to end of prevMove slice
+	var appendMove Move
+	appendMove.start = st
+	appendMove.end = en
+	appendMove.pieceMoved = cb.board[st]
+	appendMove.color = cb.nextmove
+
+	if cb.board[en] != nil {
+		cb.board[en].alive = false
+		appendMove.pieceCaptured = cb.board[en]
+		appendMove.capPos = en
+	}
+
+	// this is the naive movement, for simple movement
 	cb.board[en] = cb.board[st]
 	cb.board[st] = nil
 	cb.board[en].pos = en
+
+	// check if move was enpassant take
+	enpasMove := ( cb.board[en].piece == 1 && en == cb.enpas )
+
+	cb.enpas = -1
+
+	// check if move was a castle
+	// check if piece moved was a king
+	if cb.board[en].piece == 0 {
+		stRank := ( st & 56 ) >> 3
+		stFile := st & 7 
+		enRank := ( en & 56 ) >> 3
+		enFile := en & 7 
+
+		// changed castling permissions
+		cb.castle[2*cb.board[en].color] = false
+		cb.castle[(2*cb.board[en].color)+1] = false
+
+		// check if king is castling and to which side
+		if stRank == enRank && enFile - stFile == 2 {
+			// king is castling king side
+			// move rook to F1/F8
+			cb.board[st+1] = cb.board[en+1]
+			cb.board[st+1].pos = st+1
+			cb.board[en+1] = nil
+			appendMove.castle = (2*cb.board[en].color) + 1
+		} else if stRank == enRank && stFile - enFile == 2 {
+			// king is castling queen side
+			// move rook to D1/D8
+			cb.board[st-1] = cb.board[stRank]
+			cb.board[st-1].pos = st-1
+			cb.board[stRank] = nil
+			appendMove.castle = (2*cb.board[en].color) + 2
+		}
+	}
+
+	// check if piece moved was a rook
+	if cb.board[en].piece == 4 {
+		// changed castling permissions
+		cb.castle[2*cb.board[en].color] = false
+		cb.castle[(2*cb.board[en].color)+1] = false
+	}
+
+	// check if piece moved was a pawn
+	if cb.board[en].piece == 1 {
+		stRank := ( st & 56 ) >> 3
+		// stFile := st & 7 
+		enRank := ( en & 56 ) >> 3
+		enFile := en & 7 
+
+		// check if the move was a double push
+		if enRank - stRank == 2 || stRank - enRank == 2 {
+			cb.enpas = st + (4 * (enRank - stRank) )
+			// cb.enpas = -1
+		}
+
+		// check if move was an enpas take
+		if enpasMove {
+			enpasPawnPos := (stRank << 3) + enFile
+			/*
+				fmt.Printf("ENPAS MADNESS (%v%v)\n",(string)('A'+enFile),stRank+1)
+				cb.PrintBoard()
+				cb.PrintPieces()
+				fmt.Printf("\n\n")
+			*/
+			// fmt.Printf("%v", enpasPawnPos )
+			appendMove.pieceCaptured = cb.board[ enpasPawnPos ]
+			appendMove.capPos = enpasPawnPos
+			cb.board[ enpasPawnPos ].alive = false
+			cb.board[ enpasPawnPos ] = nil
+			// cb.enpas = -1
+		}
+	}
+
+	// add new move to slice
+	cb.prevMoves = append(cb.prevMoves, appendMove)
+}
+
+func (cb *ChessBoard) undoMove(move Move) {
+	// undo a given move on the board. This will
+	// only undo the previous move that was on the 
+	// board. If provided a different move than the
+	// immediate previous one, then could cause issues
+
+	var pieces *[16]Piece
+	lastMove := cb.prevMoves[ len(cb.prevMoves)-1 ]
+
+	if ( cb.nextmove == 0 ) {
+		pieces = &cb.black
+	} else {
+		pieces = &cb.white
+		// pieces = &cb.black
+	}
+
+	st := move.start
+	en := move.end
+
+	// reset piece on board to previous position 
+	cb.board[move.start] = cb.board[move.end]
+	cb.board[move.start].pos = move.start
+	cb.board[move.end] = nil
+
+	// loop through other side's pieces and bring
+	// back a piece if it was taken on previous turn
+	for i, p := range pieces {
+		if p.pos == lastMove.capPos {
+			// p.alive = true
+			// cb.board[move.end] = &p
+			pieces[i].alive = true
+			// cb.board[lastMove.capPos] = lastMove.pieceCaptured
+			cb.board[lastMove.capPos] = &pieces[i]
+			cb.prevMoves[len(cb.prevMoves)-1] = Move{}
+			cb.prevMoves = cb.prevMoves[:len(cb.prevMoves)-1]
+			return
+		}
+	}
+
+	// check if move was a castle
+	// check if piece moved was a king
+	if cb.board[st].piece == 0 {
+		stRank := ( st & 56 ) >> 3
+		stFile := st & 7 
+		enRank := ( en & 56 ) >> 3
+		enFile := en & 7 
+		// check if king is castling and to which side
+		if stRank == enRank && enFile - stFile == 2 {
+			// king is castling king side
+			// move rook to F1/F8
+			cb.board[en+1] = cb.board[st+1]
+			cb.board[en+1].pos = en+1
+			cb.board[st+1] = nil
+		} else if stRank == enRank && stFile - enFile == 2 {
+			// king is castling queen side
+			// move rook to D1/D8
+			cb.board[stRank] = cb.board[st-1]
+			cb.board[stRank].pos = stRank
+			cb.board[st-1] = nil
+		}
+	}
+
+	// check if piece moved was a pawn
+	if cb.board[st].piece == 1 {
+		stRank := ( st & 56 ) >> 3
+		// stFile := st & 7 
+		// enRank := ( en & 56 ) >> 3
+		enFile := en & 7 
+
+		// check if move was an enpas take
+		if en == cb.enpas {
+			// find which piece was captured and bring back to life
+			enpasPawnPos := (stRank << 3) + enFile
+			for i, p := range pieces {
+				if p.pos == (enpasPawnPos) {
+					pieces[i].alive = true
+					cb.board[enpasPawnPos] = &pieces[i]
+					/*
+						fmt.Printf("ENPAS UNDO (%v%v)\n",(string)('A'+enFile),stRank+1)
+						cb.PrintBoard()
+						cb.PrintPieces()
+						fmt.Printf("\n\n")
+					*/
+			  }
+			}
+		}
+	}
+
+	// delete last element of prevMoves
+	cb.prevMoves[len(cb.prevMoves)-1] = Move{}
+	cb.prevMoves = cb.prevMoves[:len(cb.prevMoves)-1]
+}
+
+func (cb *ChessBoard) CheckMate() bool {
+	// this function will return checkmate if the current
+	// side has no more moves to play
+
+	return len(cb.moves) == 0
+}
+
+func (cb *ChessBoard) checkAttacks() {
+	cb.attackSquares = make([]int8, 0)
+
+	var pieces *[16]Piece
+
+	if ( cb.nextmove == 0 ) {
+		pieces = &cb.black
+		//forward = 8
+	} else {
+		pieces = &cb.white
+		//forward = -8
+	}
+
+	for _, p := range pieces {
+		if !p.alive {
+			continue
+		}
+		if p.piece == 0 {
+			kingAttack( cb, p )
+		} else if p.piece == 1 {
+			pawnAttack( cb, p )
+			//cb.moves = append(cb.moves, Move{p.pos,p.pos+(uint8)(forward)})
+		} else if p.piece == 2 {
+			knightAttack( cb, p )
+		} else if p.piece == 3 {
+			bishopAttack( cb, p )
+		} else if p.piece == 4 {
+			rookAttack( cb, p )
+		} else if p.piece == 5 {
+			bishopAttack( cb, p )
+			rookAttack( cb, p )
+		}
+	}
+}
+
+func (cb *ChessBoard) inCheck() {
+	// this function will check if the king of the current side is in check
+	cb.check = false
+
+	cb.checkAttacks()
+
+	var pieces *[16]Piece
+
+	if ( cb.nextmove == 0 ) {
+		pieces = &cb.white
+		//forward = 8
+	} else {
+		pieces = &cb.black
+		//forward = -8
+	}
+
+	king := pieces[0]
+
+	for _,attacked := range cb.attackSquares {
+		if attacked == king.pos {
+			cb.check = true
+		}
+	}
 }
 
 func (cb *ChessBoard) GenMoves() {
+
+	cb.inCheck()
+
 	cb.moves = make([]Move, 0)
 
 	var pieces *[16]Piece
@@ -136,8 +482,9 @@ func (cb *ChessBoard) FENSet(str string) {
 
 	var addP *Piece
 
+	// loop through the string
 	for _, c := range str {
-		// if c == '/' 
+		// if c == '/', starting filling next rank
 		if c == '/' {
 			rank--
 			file = 0
@@ -176,6 +523,33 @@ func (cb *ChessBoard) FENSet(str string) {
 		
 	}
 
+}
+
+func (cb *ChessBoard) Perft(depth int) uint64 {
+	// this function will perform perft function to go to
+	// all moves in chess board
+
+	var nodes uint64
+	var resetEnpas int8
+
+	if ( depth == 0 ) {
+		return 1
+	}
+
+	cb.GenMoves()
+	cpyMoves := make([]Move, len( cb.moves ) )
+	copy( cpyMoves, cb.moves )
+
+	for _,m := range cpyMoves {
+		resetEnpas = cb.enpas
+		cb.makeMove(m)
+		cb.nextmove = 1 ^ cb.nextmove
+		nodes += cb.Perft( depth - 1 )
+		cb.nextmove = 1 ^ cb.nextmove
+		cb.enpas = resetEnpas
+		cb.undoMove(m)
+	}
+	return nodes
 }
 
 func initSide(color uint8) [16]Piece {
@@ -238,14 +612,83 @@ func printHorizontalLine() {
 }
 
 func (cb *ChessBoard) PrintMoves() {
+	fmt.Printf("enpas = (%v,%v)\n", (cb.enpas & 56) >> 3, cb.enpas & 7 )
+	fmt.Printf("%v\n",cb.nextmove)
 	for _,m := range cb.moves {
 		sFile := m.start & 7
 		sRank := (m.start &  56) >> 3
 		eFile := m.end & 7
 		eRank := (m.end &  56) >> 3
 
-		fmt.Printf("[%v] (%v,%v) -> (%v,%v)\n",(string)(cb.board[m.start].rep),sFile,sRank,eFile,eRank)
+		fmt.Printf("[%v] (%v%v) -> (%v%v)\n",(string)(cb.board[m.start].rep),(string)('A'+sFile),sRank+1,(string)('A'+eFile),eRank+1)
 	}
+}
+
+func (cb *ChessBoard) PrintPieces() {
+	// this function will print all of the pieces in the game
+	for _,p := range cb.white {
+		file := p.pos & 7
+		rank := (p.pos & 56) >> 3
+
+		fmt.Printf("[%v] %v%v \tAlive: %v\n",(string)(p.rep),(string)('A'+file),rank+1,p.alive)
+	}
+	fmt.Printf("\n")
+
+	for _,p := range cb.black {
+		file := p.pos & 7
+		rank := (p.pos & 56) >> 3
+
+		fmt.Printf("[%v] %v%v \tAlive: %v\n",(string)(p.rep),(string)('A'+file),rank+1,p.alive)
+	}
+
+}
+
+func (cb *ChessBoard) PrintPrevMoves() {
+	var sRank, sFile, eRank, eFile int8
+	var capRep string
+	for i,m := range cb.prevMoves {
+		sRank = ( m.start & 56 ) >> 3
+		sFile = m.start & 7
+		eRank = ( m.end & 56 ) >> 3
+		eFile = m.end & 7
+
+		if m.pieceCaptured != nil {
+			capRep = (string)(m.pieceCaptured.rep)
+		} else {
+			capRep = ""
+		}
+
+		fmt.Printf("Move %v: (%v) [%v] (%v%v) -> (%v%v) [%v]\n",i+1,m.color,(string)(m.pieceMoved.rep),(string)('A'+sFile),sRank+1,(string)('A'+eFile),eRank+1,capRep)
+	}
+}
+
+func (cb *ChessBoard) PrintCastlePriv() {
+	// this function will print the castling privileges of the board
+	if cb.castle[0] {
+		fmt.Printf("K")
+	} else {
+		fmt.Printf("-")
+	}
+
+	if cb.castle[1] {
+		fmt.Printf("Q")
+	} else {
+		fmt.Printf("-")
+	}
+
+	if cb.castle[2] {
+		fmt.Printf("k")
+	} else {
+		fmt.Printf("-")
+	}
+
+	if cb.castle[3] {
+		fmt.Printf("q")
+	} else {
+		fmt.Printf("-")
+	}
+
+	fmt.Printf("\n")
 }
 
 /*
--- main.go
+++ main.go
@@ -10,12 +10,24 @@ import (
 
 func main() {
 	// this is a comment
-	var b = board.InitBoard()
+	var b board.ChessBoard
 
-	b.FENSet("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQK2R")
+	b.InitBoard()
 
+	/*
+	n := 4
+	x := b.Perft(n)
+
+	fmt.Printf("Perft(%v): %v\n",n,x)
+	return
+	*/
+
+	// b.FENSet("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQK2R")
+	// b.FENSet("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+	b.FENSet("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R")
 
 	var nextMove string
+	badMove := false
 
 	for {
 		cmd := exec.Command("clear")
@@ -23,11 +35,32 @@ func main() {
 		cmd.Run()
 
 		b.PrintBoard()
+		b.PrintCastlePriv()
+
+		if badMove {
+			fmt.Printf("Bad move\n")
+		}
 
 		b.GenMoves()
 
+		// check for checkmate
+		if b.CheckMate() {
+			fmt.Printf("GAME OVER\n")
+			return
+		}
+
+		b.PrintPrevMoves()
+		b.PrintMoves()
+		// fmt.Printf("\n")
+		// b.PrintPieces()
+
 		fmt.Scan(&nextMove)
 
-		b.MakeMove(nextMove)
+		if b.UserMove(nextMove) {
+			b.UpdateNextMove()
+			badMove = false
+		} else {
+			badMove = true
+		}
 	}
 }
