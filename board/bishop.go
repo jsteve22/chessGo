724bbbf0dcd3c9dd8e7506d51e6f4228ed376846
jsteve22 <jsteve22@nd.edu>
2023-07-24 22:24:23 +0200

    added bishop pseudomove generator

--- board/bishop.go
+++ board/bishop.go
@@ -1,5 +1,68 @@
 package board
 
+func BishopGeneratePseudoLegalMoves(bishop Piece, game Game) []Move {
+	var moves []Move
+
+	emptySquare := uint8(0)
+	lowerMask := uint8(8 - 1)
+	// upperMask := uint8(64 - 1 - lowerMask)
+
+	rank := bishop.pos >> 3
+	file := bishop.pos & lowerMask
+
+	BOARD_SIZE := uint8(8)
+
+	r := rank + 1
+	f := file + 1
+	for (r < BOARD_SIZE) && (f < BOARD_SIZE) {
+		index := (r << 3) + f
+		if (game.board[index] != emptySquare) && (game.board[index]>>3)^bishop.color == 0 {
+			break
+		}
+		moves = append(moves, Move{start: bishop.pos, end: index})
+		r++
+		f++
+	}
+
+	r = rank - 1
+	f = file + 1
+	for (r != 255) && (f < BOARD_SIZE) {
+		index := (r << 3) + f
+		if (game.board[index] != emptySquare) && (game.board[index]>>3)^bishop.color == 0 {
+			break
+		}
+		moves = append(moves, Move{start: bishop.pos, end: index})
+		r--
+		f++
+	}
+
+	r = rank - 1
+	f = file - 1
+	for (r != 255) && (f != 255) {
+		index := (r << 3) + f
+		if (game.board[index] != emptySquare) && (game.board[index]>>3)^bishop.color == 0 {
+			break
+		}
+		moves = append(moves, Move{start: bishop.pos, end: index})
+		r--
+		f--
+	}
+
+	r = rank + 1
+	f = file - 1
+	for (r < BOARD_SIZE) && (f != 255) {
+		index := (r << 3) + f
+		if (game.board[index] != emptySquare) && (game.board[index]>>3)^bishop.color == 0 {
+			break
+		}
+		moves = append(moves, Move{start: bishop.pos, end: index})
+		r++
+		f--
+	}
+
+	return moves
+}
+
 /*
 func BishopMove(cb *ChessBoard, p Piece) {
 	var pos int8
