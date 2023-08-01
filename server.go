995fbd90b6baf048db43c12529b147ae8da8b559
jsteve22 <jsteve22@nd.edu>
2023-08-01 23:53:20 +0200

    edit server to have simple API for move generation

--- server.go
+++ server.go
@@ -1,13 +1,45 @@
-// package main
+package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/jsteve22/chessGo/board"
 )
 
+type Response struct {
+	Data []board.JsonMove
+}
+
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "hello world")
+	data := make([]board.JsonMove, 0)
+	length := r.ContentLength
+	fmt.Printf("body length: %v\n", length)
+
+	if length > 0 {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		respBytes := buf.String()
+		fen := string(respBytes)
+		fmt.Printf("body content: %s\n", fen)
+		// data = strings.ToLower(data)
+		game := board.LoadBoard(fen)
+		moves := board.GenerateMoves(game)
+		board.PrintMoves(game, moves)
+		data = board.GetJsonMoves(moves)
+	}
+	fmt.Printf("\n")
+
+	header := w.Header()
+	header.Set("Access-Control-Allow-Origin", "*")
+
+	response := Response{Data: data}
+	marshalled, err := json.Marshal(response)
+	// write response and send back to client
+	_, err = w.Write(marshalled)
 	checkError(err)
 }
 
@@ -20,10 +52,10 @@ func checkError(err error) {
 func main() {
 	fs := http.FileServer(http.Dir("./chessfrontend/dist"))
 	http.Handle("/", fs)
+	http.HandleFunc("/FEN", homePageHandler)
 
-
-	fmt.Printf("Starting server on port 8080\n")
+	fmt.Printf("Starting server on port 12345\n")
 	log.Panic(
-		http.ListenAndServe(":80", nil),
+		http.ListenAndServe(":12345", nil),
 	)
 }
